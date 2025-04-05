6fe376d9c01e261c817ae39145af018b317f8736
jhua0159 <jhua0159@student.monash.edu>
2025-04-05 17:32:36 +1100

    add requirement.txt and change mistake in main.js

--- fit5120-vue/src/main.js
+++ fit5120-vue/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
-import 'leaflet/dist/leaflet.css';
+
 
 const app = createApp(App);
 
