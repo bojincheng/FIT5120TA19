8ad9161af7d4c3e6059858f162c693c36110225c
bche0078 <bche0078@student.monash.edu>
2025-05-08 13:53:05 +1000

    Mobile-responsive logic testing

--- fit5120-vue/src/App.vue
+++ fit5120-vue/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <router-view></router-view>
 </template>
 
@@ -14,4 +14,40 @@ body {
   padding: 0;
   font-family: Arial, sans-serif;
 }
-</style> 
+</style>  -->
+
+# Mobile device check
+<template>
+  <div>
+    <BeachSafety v-if="isMobile" />
+    <router-view v-else />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import BeachSafety from './BeachSafety.vue'
+
+const isMobile = ref(false)
+
+const checkDevice = () => {
+  isMobile.value = window.innerWidth < 768
+}
+
+onMounted(() => {
+  checkDevice()
+  window.addEventListener('resize', checkDevice)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkDevice)
+})
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
