47841529d6a5d068c7c6e21e79d86f0857708b81
jhua0159 <jhua0159@student.monash.edu>
2025-05-23 00:47:32 +1000

    change back backend url

--- FIT5120-VUE/src/components/BeachComparisonTool.vue
+++ FIT5120-VUE/src/components/BeachComparisonTool.vue
@@ -1040,15 +1040,15 @@ export default {
           // Now process with the API using the created file
           const formData = new FormData();
           formData.append('image', this.imageFile);
-          response = await axios.post('http://127.0.0.1:5001/process-image', formData);
+          response = await axios.post('https://fit5120ta19.onrender.com/process-image', formData);
         } 
         // Process normally if we have local file or captured image
         else if (this.imageFile) {
           const formData = new FormData();
           formData.append('image', this.imageFile);
-          response = await axios.post('http://127.0.0.1:5001/process-image', formData);
+          response = await axios.post('https://fit5120ta19.onrender.com/process-image', formData);
         } else if (this.capturedImage) {
-          response = await axios.post('http://127.0.0.1:5001/process-camera-image', {
+          response = await axios.post('https://fit5120ta19.onrender.com/process-camera-image', {
             image: this.capturedImage
           });
         } else {
@@ -1276,7 +1276,7 @@ export default {
         this.analyzeRipLoading = false;
       };
 
-      img.src = `http://127.0.0.1:5001${result.original}`;
+      img.src = `https://fit5120ta19.onrender.com${result.original}`;
     },
     
     convertMsToKmh(speedMs) {
@@ -1658,8 +1658,8 @@ export default {
       
       try {
         // API endpoints
-        const weatherUrl = `http://127.0.0.1:5001/weather?latitude=${lat}&longitude=${lon}`;
-        const marineUrl = `http://127.0.0.1:5001/marine?latitude=${lat}&longitude=${lon}`;
+        const weatherUrl = `https://fit5120ta19.onrender.com/weather?latitude=${lat}&longitude=${lon}`;
+        const marineUrl = `https://fit5120ta19.onrender.com/marine?latitude=${lat}&longitude=${lon}`;
         
         console.log('Requesting weather data from:', weatherUrl);
         console.log('Requesting marine data from:', marineUrl);
